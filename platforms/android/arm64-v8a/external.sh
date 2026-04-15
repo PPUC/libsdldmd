80eede71370ac19de1a1fe0559e424e2cccc48b2
Markus Kalkbrenner <git@kalki.de>
2026-04-15 12:33:18 +0200

    android like vpinball

--- platforms/android/arm64-v8a/external.sh
+++ platforms/android/arm64-v8a/external.sh
@@ -46,7 +46,7 @@ cmake \
    -DSDL_SHARED=ON \
    -DSDL_STATIC=OFF \
    -DSDL_TEST_LIBRARY=OFF \
-   -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_LATEST_HOME}/build/cmake/android.toolchain.cmake" \
+   -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake" \
    -DCMAKE_SYSTEM_NAME=Android \
    -DCMAKE_SYSTEM_VERSION=30 \
    -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
