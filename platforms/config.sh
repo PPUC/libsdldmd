5c35d79f0c2e87b22348cd0e257321956ad8475c
Markus Kalkbrenner <git@kalki.de>
2026-06-13 17:10:18 +0200

    updated libdmdutil

--- platforms/config.sh
+++ platforms/config.sh
@@ -3,7 +3,7 @@
 set -e
 
 SDL_SHA=5848e584a1b606de26e3dbd1c7e4ecbc34f807a6
-LIBDMDUTIL_SHA=a3c9809b8e407ac57de4ebbb3d470fcebaa805cc
+LIBDMDUTIL_SHA=734a0bddc05f72bd560966a1b74b982c2191d976
 
 if [ -z "${BUILD_TYPE}" ]; then
    BUILD_TYPE="Release"
